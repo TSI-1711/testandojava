ea5a8e51ac523d524e0c1a85461516fba4742c23
IsaqTeste <isaqmelo40@gmail.com>
2025-05-23 15:57:00 -0400

    Testando meu outro email

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        system.out.println("TESTETSTESTETSTE");
+        System.out.println("TESTETSTESTETSTE");
+        System.out.println("Testando pelo email isaqmelo40");
     }
 }
